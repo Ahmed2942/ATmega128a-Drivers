2d3ae3e882b90d39592661b1141f98db877ea6b3
Ahmed2942 <67532340+Ahmed2942@users.noreply.github.com>
2023-03-23 02:02:48 +0200

    Add files via upload

---
+++ atmega128a/atmega128a/Std_types.h
@@ -0,0 +1,67 @@
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+/*************** STD_TYPES.h ******************/
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef char c8;
+
+typedef unsigned short u16;
+typedef signed short s16;
+
+typedef unsigned long u32;
+typedef signed long s32;
+
+typedef unsigned long long u64;
+typedef signed long long s64;
+
+typedef unsigned char* p8;
+typedef unsigned short* p16;
+typedef unsigned long* p32;
+typedef unsigned long long* p64;
+
+typedef float f32;
+typedef double f64;
+
+#define NULL (0)
+#define NULLPTR ((void*)0)
+
+typedef enum {
+	OK,
+	NOK,
+	OUT_OF_RANGE
+} Error_t;
+
+typedef enum {
+	FALSE,
+	TRUE
+} bool_t;
+
+#define MAX_U8 ((u8)255)
+#define MIN_U8 ((u8)0)
+
+#define MAX_S8 ((s8)127)
+#define MIN_S8 ((s8)-128)
+
+#define MAX_U16 ((u16)65535)
+#define MIN_U16 ((u16)0)
+
+#define MAX_S16 ((s16)32767)
+#define MIN_S16 ((s16)-32768)
+
+#define MAX_U32 ((u32)4294967295)
+#define MIN_U32 ((u32)0)
+
+#define MAX_S32 ((s32)2147483647)
+#define MIN_S32 ((s32)-2147483648)
+
+#define MAX_U64 ((u64)18446744073709551615)
+#define MIN_U64 ((u64)0)
+
+#define MAX_S64 ((s64)9223372036854775807)
+#define MIN_S64 ((s64)-9223372036854775808)
+
+
+
+#endif /* STD_TYPES_H_ */
--- atmega128a/atmega128a/main.c
+++ atmega128a/atmega128a/main.c
@@ -1,13 +1,85 @@
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include "MemMap.h"
+#include "Std_types.h"
+#include "UTILS.h"
+#include "ADC.h"
+#include "LCD.h"
+#include "I2C.h"
+#include "MAX30102.h"
+#include "heartRate.h"
 
 
-#include <avr/io.h>
+const u8 RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
+u8 rates[4]; //Array of heart rates
+u8 rateSpot = 0;
+u32 lastBeat = 0; //Time at which the last beat occurred
 
+float beatsPerMinute;
+int beatAvg;
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
+	DIO_Init();
+    LCD_Init();
+    LCD_GoTo(0,0);
+    LCD_Writestring("Init.");
+	_delay_ms(1000);
+    // Initialize sensor
+    if (!Begin(I2C_SPEED_FAST, MAX30102_ADDRESS)) //Use 400kHz speed
     {
+		LCD_GoTo(0,0);
+	    LCD_Writestring("MAX30102 was not found. Please check wiring/power. ");
+	    while (1);
+    }
+    LCD_Writestring("Place your finger on the sensor with steady pressure.");
+
+    //setup(); //Configure sensor with default settings
+    setup(0x1F, 4, 3, 400, 411, 4096);
+    setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+    while (1) 
+    {
+		u32 irValue = getIR();//  s / u
+
+	    if (checkForBeat(irValue) == TRUE)
+	    {
+		    //We sensed a beat!
+		    u32 delta = millis() - lastBeat;
+		    lastBeat = millis();
+
+		    beatsPerMinute = 60 / (delta / 1000.0);
+
+		    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+		    {
+			    rates[rateSpot++] = (u8)beatsPerMinute; //Store this reading in the array
+			    rateSpot %= RATE_SIZE; //Wrap variable
+
+			    //Take average of readings
+			    beatAvg = 0;
+			    for (u8 x = 0 ; x < RATE_SIZE ; x++)
+			    beatAvg += rates[x];
+			    beatAvg /= RATE_SIZE;
+		    }
+	    }
+		LCD_GoTo(0,0);
+	    LCD_Writestring("IR=");
+		LCD_GoTo(1,0);
+	    LCD_WriteNumber(irValue);//s32
+		_delay_ms(10000);
+		LCD_GoTo(0,0);
+	    LCD_Writestring(", BPM=");
+		LCD_GoTo(1,0);
+	    LCD_Write_Float(beatsPerMinute);
+		_delay_ms(10000);
+		LCD_GoTo(0,0);
+	    LCD_Writestring(", Avg BPM=");
+		LCD_GoTo(1,0);
+	    LCD_WriteNumber(beatAvg);// int
+		_delay_ms(10000);
+		
+	    if (irValue < 50000)
+	    LCD_Writestring(" No finger?");
+
     }
 }
 
